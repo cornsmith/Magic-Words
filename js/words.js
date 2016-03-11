2a4029aa9bc896ed6ab7c3042d1154758ac799ac
cornsmith <corn.smith@outlook.com>
2016-03-11 21:41:29 +1100

    First commit

---
+++ js/words.js
@@ -0,0 +1,50 @@
+var wordsYellow = [
+"a   ",
+"and ",
+"be  ",
+"I   ",
+"in  ",
+"is  ",
+"it  ",
+"of  ",
+"that",
+"the ",
+"to  ",
+"was "
+];
+
+var wordsBlue = [
+"an",
+"by",
+"do",
+"go",
+"if",
+"me",
+"my",
+"no",
+"or",
+"up"
+];
+
+var wordsRed = [
+"all ",
+"are ",
+"as  ",
+"at  ",
+"but ",
+"for ",
+"had ",
+"have",
+"he  ",
+"her ",
+"his ",
+"not ",
+"on  ",
+"one ",
+"said",
+"so  ",
+"they",
+"we  ",
+"with",
+"you "
+];
