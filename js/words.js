e5aaee0b7159f5dfa0d7ee762013c8aec258f164
cornsmith <corn.smith@outlook.com>
2016-03-24 21:34:07 +1100

    Revert "Change to shuffle"
    This reverts commit 934fd0cfc9262e3aa35970eddadb101b8950adf2.

--- js/words.js
+++ js/words.js
@@ -1,143 +1,107 @@
 var wordsets = {
-		"yellow": [
-			"a   ",
-			"and ",
-			"be  ",
-			"I   ",
-			"in  ",
-			"is  ",
-			"it  ",
-			"of  ",
-			"that",
-			"the ",
-			"to  ",
-			"was "
-		],
-		"blue": [
-			"an",
-			"by",
-			"do",
-			"go",
-			"if",
-			"me",
-			"my",
-			"no",
-			"or",
-			"up"
-		],
-		"red": [
-			"all ",
-			"are ",
-			"as  ",
-			"at  ",
-			"but ",
-			"for ",
-			"had ",
-			"have",
-			"he  ",
-			"her ",
-			"his ",
-			"not ",
-			"on  ",
-			"one ",
-			"said",
-			"so  ",
-			"they",
-			"we  ",
-			"with",
-			"you "
-		],
-		"green": [
-			"big",
-			"can",
-			"did",
-			"get",
-			"has",
-			"him",
-			"new",
-			"now",
-			"off",
-			"old",
-			"our",
-			"out",
-			"see",
-			"she",
-			"two",
-			"who"
-		],
-		"orange": [
-			"back",
-			"been",
-			"came",
-			"down",
-			"from",
-			"into",
-			"just",
-			"like",
-			"made",
-			"much",
-			"over",
-			"them",
-			"this",
-			"well",
-			"went",
-			"when"
-		]
-	};
-
-function shuffle(array) {
-	let counter = array.length;
-
-	// While there are elements in the array
-	while (counter > 0) {
-			// Pick a random index
-			let index = Math.floor(Math.random() * counter);
-
-			// Decrease counter by 1
-			counter--;
-
-			// And swap the last element with it
-			let temp = array[counter];
-			array[counter] = array[index];
-			array[index] = temp;
-	}
-
-	return array;
-}
+	"yellow": [
+		"a   ",
+		"and ",
+		"be  ",
+		"I   ",
+		"in  ",
+		"is  ",
+		"it  ",
+		"of  ",
+		"that",
+		"the ",
+		"to  ",
+		"was "
+	],
+	"blue": [
+		"an",
+		"by",
+		"do",
+		"go",
+		"if",
+		"me",
+		"my",
+		"no",
+		"or",
+		"up"
+	],
+	"red": [
+		"all ",
+		"are ",
+		"as  ",
+		"at  ",
+		"but ",
+		"for ",
+		"had ",
+		"have",
+		"he  ",
+		"her ",
+		"his ",
+		"not ",
+		"on  ",
+		"one ",
+		"said",
+		"so  ",
+		"they",
+		"we  ",
+		"with",
+		"you "
+	],
+	"green": [
+		"big",
+		"can",
+		"did",
+		"get",
+		"has",
+		"him",
+		"new",
+		"now",
+		"off",
+		"old",
+		"our",
+		"out",
+		"see",
+		"she",
+		"two",
+		"who"
+	],
+	"orange": [
+		"back",
+		"been",
+		"came",
+		"down",
+		"from",
+		"into",
+		"just",
+		"like",
+		"made",
+		"much",
+		"over",
+		"them",
+		"this",
+		"well",
+		"went",
+		"when"
+	]
+};
 
 function changeWord() {
-	// Pick random word
-	// var word = words[Math.floor(Math.random() * words.length)];
-
-	// Get and pop last word
-	var word = words.pop();
-
-	// Smiley if finished
-	if (word) {
+		var word = words[Math.floor(Math.random() * words.length)];
 		document.getElementById("word").innerHTML = word;
-	} else {
-		document.getElementById("word").innerHTML = "&#9786;";
-	};
-}
-
-function toggleColour(elem) {
-	// Get the buttons
-	var x = document.getElementsByClassName("square");
-
-	// Highlight active button
-	var i;
-	for (i = 0; i < x.length; i++) {
-		x[i].style.borderColor = "white";
+		//document.getElementById("word").style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16);
 	}
-	elem.style.borderColor = "black";
 
-	// Initialise wordset
-	words = wordsets[elem.id];
-	shuffle(words);
-	changeWord();
-}
+	function toggleColour(elem) {
+		var x = document.getElementsByClassName("square");
+		var i;
+		for (i = 0; i < x.length; i++) {
+			x[i].style.borderColor = "white";
+		}
+		elem.style.borderColor = "black";
+		words = wordsets[elem.id];
+		changeWord();
+	}
 
 var words = wordsets["yellow"];
-shuffle(words);
 document.getElementById("yellow").style.borderColor = "black";
-changeWord();
+changeWord()
