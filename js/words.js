95e3a8e3330750e6f36c149fdc200d6870e655e0
cornsmith <corn.smith@outlook.com>
2016-03-25 20:31:02 +1100

    Incremental update towards shuffle

--- js/words.js
+++ js/words.js
@@ -85,22 +85,57 @@ var wordsets = {
 	]
 };
 
-function changeWord() {
-		var word = words[Math.floor(Math.random() * words.length)];
-		document.getElementById("word").innerHTML = word;
-		//document.getElementById("word").style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16);
+function shuffle(array) {
+	let counter = array.length;
+
+	// While there are elements in the array
+	while (counter > 0) {
+			// Pick a random index
+			let index = Math.floor(Math.random() * counter);
+
+			// Decrease counter by 1
+			counter--;
+
+			// And swap the last element with it
+			let temp = array[counter];
+			array[counter] = array[index];
+			array[index] = temp;
 	}
 
-	function toggleColour(elem) {
-		var x = document.getElementsByClassName("square");
-		var i;
-		for (i = 0; i < x.length; i++) {
-			x[i].style.borderColor = "white";
-		}
-		elem.style.borderColor = "black";
-		words = wordsets[elem.id];
-		changeWord();
+	return array;
+}
+
+function changeWord() {
+	// Pick random word
+	var word = words[Math.floor(Math.random() * words.length)];
+
+	// Get and pop last word
+	//var word = words.pop();
+
+	// Smiley if finished
+	if (word) {
+		document.getElementById("word").innerHTML = word;
+	} else {
+		document.getElementById("word").innerHTML = "&#9786;";
+	};
+}
+
+function toggleColour(elem) {
+	// Get the buttons
+	var x = document.getElementsByClassName("square");
+
+	// Highlight active button
+	var i;
+	for (i = 0; i < x.length; i++) {
+		x[i].style.borderColor = "white";
 	}
+	elem.style.borderColor = "black";
+
+	// Initialise wordset
+	words = wordsets[elem.id];
+	//shuffle(words);
+	changeWord();
+}
 
 var words = wordsets["yellow"];
 document.getElementById("yellow").style.borderColor = "black";
